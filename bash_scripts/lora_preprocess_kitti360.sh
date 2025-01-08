6d79f40899dd21ee2f0cf65ab96020d17106e479
ppadjin <pavle.padjin@gmail.com>
2025-01-08 17:35:57 +0000

    adding data root to override the random default data dir

--- bash_scripts/lora_preprocess_kitti360.sh
+++ bash_scripts/lora_preprocess_kitti360.sh
@@ -1,2 +1,2 @@
-python lora/scripts/preprocess_kitti360_for_lora.py --n_images 100 \
+python lora/scripts/preprocess_kitti360_for_lora.py --data_root ${KITTI_DIR} --n_images 100 \
                                                 --out_dir "lora/data/kitti360"
